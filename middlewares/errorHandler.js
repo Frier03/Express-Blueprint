d345c70d4dd16b982c984049f979de0d298ddac5
Sh1tters <justinbestworldwide@gmail.com>
2023-04-08 22:24:07 +0200

    Implement logger.error
    When an error occurs, it will not log the error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,9 @@
+const logger = require('../utils/logger')
+
 function errorHandler(err, req, res, next) {
   const statusCode = err.statusCode || 500; // default if no status code provided
+
+  logger.error(`Error occured with status code ${statusCode}`, { ...err, action: 'Error'})
   res.status(statusCode).send(err.message)
   }
   
