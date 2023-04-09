26fdd2f13cdc6761fba846bb66314283c119fd05
Sh1tters <justinbestworldwide@gmail.com>
2023-04-09 21:03:14 +0200

    Update index to support authMiddleware into 'auth0/login'

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const logger = require('./utils/logger')
 const { errorHandler } = require('./middlewares/errorHandler')
+const { authMiddleware } = require('./middlewares/authMiddleware')
+const { validateInput } = require('./middlewares/inputValidation')
 const authRouter = require('./routes/auth0')
 
 const app = express()
 
 app.use(express.json());
+app.use('/api/auth0/login', validateInput(['username', 'password']), authMiddleware)
 app.use('/api/auth0/', authRouter)
-app.use(errorHandler)
 
+app.use(errorHandler)
 
 app.listen(8888, () => {
     logger.info(`Server started listening`, { port: 8888, action: 'Started server' })
