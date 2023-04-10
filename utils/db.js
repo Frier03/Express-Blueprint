00e905acf9d289d4f4abc4237465e3788b424fa5
Sh1tters <justinbestworldwide@gmail.com>
2023-04-10 16:54:25 +0200

    Create db.js

---
+++ utils/db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql')
+const config = require('../config.json')
+
+const pool = mysql.createPool({...})
+
+function getConnection(){
+    return pool
+}
+
+module.exports = {
+    getConnection,
+}
