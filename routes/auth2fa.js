761d2129de7f1df7e4c9e2affd839c4c6a1c1ae8
Sh1tters <justinbestworldwide@gmail.com>
2023-04-10 17:55:53 +0200

    Update auth2fa.js

--- routes/auth2fa.js
+++ routes/auth2fa.js
@@ -1,10 +1,14 @@
 const express = require('express')
 
+const otpServce = require('../services/otpService')
 const config = require('../config.json')
 const logger = require('../utils/logger')
 const router = express.Router()
 
-router.post('/otp/generate', (req, res) => {});
+router.post('/otp/generate', (req, res) => {
+    const otp = otpServce.generateOTP()
+    res.status(200).send(otp)
+});
 
 router.post('/otp/verify', (req, res) => {});
 
