41ca32cefc5c807c5eb5263830feda2a33f1eecf
Sh1tters <justinbestworldwide@gmail.com>
2023-04-10 16:54:40 +0200

    Update syntax for config.issuer

--- utils/jwt.js
+++ utils/jwt.js
@@ -8,7 +8,7 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "defaultkey"
 function createToken(payload, options = {}) {
     const defaultOptions = { expiresIn: '1h' };
     const tokenOptions = { ...defaultOptions, ...options };
-    const tokenPayload = { sub: payload.sub, iss: config.Issuer, ...payload };
+    const tokenPayload = { sub: payload.sub, iss: config.issuer, ...payload };
 
     const token = jwt.sign(tokenPayload, JWT_SECRET_KEY, tokenOptions);
     return token;
