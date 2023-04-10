27cef47bba84b6b652728e0d65aeb2028f14a6f3
Sh1tters <justinbestworldwide@gmail.com>
2023-04-10 17:55:49 +0200

    Update auth0 with simple 2FA logic

--- routes/auth0.js
+++ routes/auth0.js
@@ -1,12 +1,33 @@
 const express = require('express')
+const axios = require('axios')
 
 const config = require('../config.json')
 const logger = require('../utils/logger')
 const jwt = require('../utils/jwt')
 const router = express.Router()
 
-router.post('/login', (req, res) => {
+// Create an Axios instance with a base URL and port
+const instance = axios.create({
+  baseURL: 'http://localhost:8888'
+});
+
+router.post('/login', async(req, res) => {
     const { username, password } = req.body;
+
+    const has2faEnabled = true
+    if (has2faEnabled){ // Check if user has 2FA enabled, generate OTP
+      try {
+        logger.debug(`Generating OTP code for ${username}`, { ...otp, action: 'Generate OTP'})
+        const response = await instance.post('/api/0auth2/otp/generate', { username });
+        const otp = response.data
+        const code = otp.code
+        const createdAt = otp.createdAt
+
+        // Await verification for OTP
+      } catch (error) {
+        res.status(500).send(error)
+      }
+    }
 
     // Create and sign a JWT
     const payload = { 'sub': username };
