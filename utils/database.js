b12a64411961946ebd8932dc1b4584a0d555bbd7
Sh1tters <justinbestworldwide@gmail.com>
2023-04-12 19:35:48 +0200

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,16 +1,18 @@
 const sqlite3 = require('sqlite3');
 const path = require('path');
 const fs = require('fs');
+
 const config = require('../config.json')
+const logger = require('../utils/logger')
 
 let db;
 
 function connect() {
     db = new sqlite3.Database(config.sql.database_file, (err) => {
       if (err) {
-        console.error(err.message);
+        logger.error(`Unable to connect to database.`)
       } else {
-        console.log('Connected to the database.');
+        logger.info('Connected to the database.');
       }
     });
   }
@@ -22,10 +24,10 @@ function runSql(sqlFileName, params = []) {
     return new Promise((resolve, reject) => {
         db.run(sql, params, function(err) {
             if (err) {
-                console.error(err.message);
+                logger.error(`Unable to execute SQL command '${sqlFileName}' | Error '${err.message}'.`)
                 reject(err);
             } else {
-                console.log(`SQL command '${sqlFileName}' executed successfully.`);
+                logger.debug(`SQL command '${sqlFileName}' executed successfully.`);
                 resolve(this);
             }
         });
