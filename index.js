bf310ba9c776627e3c1884e58c9da9d6dbdff442
Sh1tters <justinbestworldwide@gmail.com>
2023-04-12 18:06:21 +0200

    Updated register for input validation

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require('express')
 
-const db = require('./utils/db')
-const logger = require('./utils/logger')
 const { errorHandler } = require('./middlewares/errorHandler')
 const { authMiddleware } = require('./middlewares/authMiddleware')
 const { validateInput } = require('./middlewares/inputValidation')
 const authRouter = require('./routes/auth0')
+const logger = require('./utils/logger')
 
 const app = express()
-const pool = db.getConnection()
-console.log(pool)
 
 app.use(express.json());
 app.use('/api/auth0/login', validateInput(['username', 'password']), authMiddleware)
+app.use('/api/auth0/register', validateInput(['mail', 'username', 'password']))
 app.use('/api/auth0/', authRouter)
 
 app.use(errorHandler)
