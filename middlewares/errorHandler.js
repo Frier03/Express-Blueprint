19060b8d4de9d7953d0f1e0fb863259c55c5a56e
Sh1tters <justinbestworldwide@gmail.com>
2023-04-13 23:37:07 +0200

    Update .send() -> .json()

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,7 +4,7 @@ function errorHandler(err, req, res, next) {
   const statusCode = err.statusCode || 500; // default if no status code provided
 
   logger.error(`Error occured with status code ${statusCode}`, { err, action: 'Error'})
-  res.status(statusCode).send(err.message)
+  res.status(statusCode).json({ message: err.message })
   }
   
   module.exports = { errorHandler };
