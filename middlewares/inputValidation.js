b7337ddc6cc07a4c8c42d8e29f0e4d71d24761b3
Sh1tters <justinbestworldwide@gmail.com>
2023-04-16 08:31:01 +0200

    implemented authzMiddleware. Fixed name conventions and made code more prettier

--- middlewares/inputValidation.js
+++ middlewares/inputValidation.js
@@ -5,20 +5,20 @@ function validateInput(requiredFields) {
     const numInputFields = Object.keys(input).length;
 
     if (!input || typeof input !== 'object') {
-      return next({ message: `Invalid request body`, statusCode: 400 });
+      return res.status(400).json({ message: `Invalid request body` });
     }
     if (numInputFields !== numRequiredFields) {
-      return next({ message: `Expected ${numRequiredFields} fields, but received ${numInputFields}`, statusCode: 400 });
+      return res.status(400).json({ message: `Expected ${numRequiredFields} fields, but received ${numInputFields}` });
     }
     for (const field of requiredFields) {
       if (!input.hasOwnProperty(field)) {
-        return next({ message: `Missing '${field}' field`, statusCode: 400 });
+        return res.status(400).json({ message: `Missing '${field}' field` });
       }
       if (typeof input[field] !== 'string' || input[field].trim() === '') {
-        return next({ message: `Invalid '${field}' field type`, statusCode: 400 });
+        return res.status(400).json({ message: `Invalid '${field}' field type` });
       }
     }
-    next();
+    return next();
   };
 }
 
